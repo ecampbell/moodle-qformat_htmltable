028e1b1ef348fddafbc4c59435bbd67d2512c820
Eoin Campbell <campbeeo@tcd.ie>
2014-01-28 15:46:21 +0000

    Create qformat_htmltable.php

---
+++ lang/en/qformat_htmltable.php
@@ -0,0 +1,16 @@
+<?php
+$string['pluginname'] = 'HTML review table format';
+$string['pluginname_help'] = 'HTML review table is a format suitable for quick review of large numbers of questions.';
+$string['pluginname_link'] = 'qformat/htmltable';
+$string['htmltable'] = 'HTML review table format';
+$string['htmltable_help'] = 'HTML review table is a format suitable for quick review of large numbers of questions.';
+$string['noquestions'] = 'No questions to export';
+$string['templateunavailable'] = 'XHTML template <b>$a</b> is not available';
+$string['xsltunavailable'] = 'You need the XSLT library installed in PHP to save this HTML file';
+$string['transformationfailed'] = 'XSLT transformation failed (<b>$a</b>)';
+$string['stylesheetunavailable'] = 'XSLT Stylesheet <b>$a</b> is not available';
+$string['cannotopentempfile'] = 'Cannot open temporary file <b>$a</b>';
+$string['cannotwritetotempfile'] = 'Cannot write to temporary file <b>$a</b>';
+$string['export_type_set'] = 'The question export type has been set to ';
+
+?>
